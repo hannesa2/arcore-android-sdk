1f8bb0308f96963fbadc825b7dba44bbd6d30c8e
Hannes Achleitner <hannes.software@gmx.at>
2022-11-09 13:27:49 +0100

    Debug info

--- samples/ml_kotlin/app/src/main/java/com/google/ar/core/examples/kotlin/ml/AppRenderer.kt
+++ samples/ml_kotlin/app/src/main/java/com/google/ar/core/examples/kotlin/ml/AppRenderer.kt
@@ -71,6 +71,7 @@ class AppRenderer(val activity: MLActivity) : DefaultLifecycleObserver, SampleRe
     val applicationInfo =
       activity.packageManager.getApplicationInfo(activity.packageName, PackageManager.GET_META_DATA)
     val apiKey = applicationInfo.metaData.getString("com.google.ar.core.examples.kotlin.ml.API_KEY")
+    Log.d("apiKey", apiKey.toString().take(1))
     if (apiKey == null) null else GoogleCloudVisionDetector(activity, apiKey)
   }
 
